33702a888b018f578fa62f48ba8bde300e1e21fb
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2022-09-08 22:47:58 +0300

    master: 1.3 - send html

--- Ch_01/01_03/start/request.js
+++ Ch_01/01_03/start/request.js
@@ -1,8 +1,10 @@
 const http = require("http");
 
 http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`
+    <h1>Hello World</h1>
+  `);
 }).listen(3000);
 
 console.log('Web server listening on port %d', 3000)
